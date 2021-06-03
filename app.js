fade04b70624a46e6b1f6c9bee19512d72dfa030
zulvkr <ivan.zulf.ikar@gmail.com>
2021-06-03 20:52:07 +0700

    alrted

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!")
+import axios from 'axios'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('form')
+  form.addEventListener('submit', event => {
+    event.preventDefault()
+    const username = document.querySelector('input').value
+    const response = axios.get(`https://api.github.com/users/${username}`)
+    console.log(response.data)
+  })
+})
